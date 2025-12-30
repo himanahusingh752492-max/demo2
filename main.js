20403f4b988b9e41b07d37590d24be18f5521aa2
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2025-12-30 14:48:32 +0530

    add home page

--- main.js
+++ main.js
@@ -8,15 +8,16 @@ let path =require('path');
 let mongoose=require("mongoose");
 let model =require("./DB");
 let bodyparser = require("body-parser");
+const bcrypt = require("bcrypt");
 app.use(ex.static(path.join(__dirname,"public")))
 mongoose.connect(process.env.DBURL).then(console.log("connected to DATABASE - " + process.env.DBURL));
 
 app.get("/",(req,res)=>{
    res.sendFile(__dirname+ '/public/index.html');
- 
+    res.sendFile(__dirname+ '/public/index1.html');
 
 })
-
+//SIGNUP
 app.post('/submit',async(req,res)=>{
     //console.log(req.body);
    
@@ -25,7 +26,7 @@ app.post('/submit',async(req,res)=>{
 try {
     const user = new model({ username, email, password });
     await user.save();
-    res.send("User registered successfully! "+ `${username}`);
+     res.redirect('/home')
  
    // res.redirect('/home');
     
@@ -37,11 +38,38 @@ try {
 
 
 })
-// app.get('/home',(req,res)=>{
-//       const username = req.query.username; 
-      
-//       res.send(`User registered successfully! ${username} `);
-// })
+//LOGIN
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body||{};
+      console.log(req.body);
+  const user = await model.findOne({email});
+     console.log( user );
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    if (user.password !== password) {
+  return res.status(400).json({ message: "Invalid password" });
+}
+   res.redirect('/home')
+  
+  } 
+  
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+//HOME
+app.get('/home',async (req, res) => {
+
+  res.sendFile(__dirname + '/public/home.html');
+
+});
+
 app.listen(8000,()=>{
     console.log("connected to SERVER - "+ process.env.PORT);
 });
---
+++ middleware.js
@@ -0,0 +1,7 @@
+//PROCESSING
+let math=math.random(1-9999);
+let myToken;
+if(myToken==math){
+
+}
+next;
